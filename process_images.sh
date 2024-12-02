903eba9bec3c0c08e6433a9dcd5257a14010f586
Daniel Herman <daniel.herman@protonmail.com>
2024-12-02 14:41:36 +0100

    Converting images to pngs

---
+++ process_images.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+set -e 
+# Ensure ImageMagick is installed
+if ! command -v convert &> /dev/null; then
+    echo "ImageMagick is not installed. Please install it to proceed."
+    exit 1
+fi
+
+# Specify the image directory
+IMAGE_DIR="./src/assets"
+FIXED_IMAGE_DIR="./src/assets/fixed"
+RAW_IMAGE_DIR="./src/assets/raw"
+
+# Create directories for processed images
+mkdir -p "$FIXED_IMAGE_DIR"
+mkdir -p "$RAW_IMAGE_DIR"
+
+# Limit for the dimensions
+LIMIT=1526
+
+IMAGE_LIST=$(find "$IMAGE_DIR" -type f -name "*.jpg" -o -name "*.jpeg" -o -name "*.png" -o -name "*.webp" -o -name "*.gif" -o -name "*.svg")
+echo $IMAGE_LIST
+for image in $IMAGE_LIST
+do
+    if [ ! -f "$image" ]; then
+        continue
+    fi
+
+    # Get current dimensions
+    dimensions=$(identify -format "%wx%h" "$image")
+    width=$(echo "$dimensions" | cut -d'x' -f1)
+    height=$(echo "$dimensions" | cut -d'x' -f2)
+
+    # Determine if resizing is necessary
+    if [ "$width" -gt "$LIMIT" ] || [ "$height" -gt "$LIMIT" ]; then
+        echo "Resizing $image with dimensions $dimensions..."
+
+        # Determine scaling factor based on larger dimension
+        if [ "$width" -gt "$height" ]; then
+            # Resize based on width
+            scale="$LIMIT"
+            magick "$image" -resize "${scale}x" "$FIXED_IMAGE_DIR/$(basename "${image%.*}").png"
+        else
+            # Resize based on height
+            scale="$LIMIT"
+            magick "$image" -resize "x${scale}" "$FIXED_IMAGE_DIR/$(basename "${image%.*}").png"
+        fi
+    else
+        echo "Copying $image without resizing..."
+        magick "$image" "$FIXED_IMAGE_DIR/$(basename "${image%.*}").png"
+    fi
+done
+
+echo "All images processed. Replacing original images."
+
+set +e
+mv "$IMAGE_DIR"/*.{jpg,jpeg,png,webp,gif,svg} "$RAW_IMAGE_DIR" 2>/dev/null
+set -e
+
+echo "Moving processed images to the image directory."
+for image in "$FIXED_IMAGE_DIR"/*.png
+do
+    mv "$image" "$IMAGE_DIR"
+done
+
+rm -rf "$FIXED_IMAGE_DIR"
+rm -rf "$RAW_IMAGE_DIR"
+
+echo "Process complete."
--- src/content/docs/recipes/standplatz.md
+++ src/content/docs/recipes/standplatz.md
@@ -6,6 +6,6 @@ description: Example of belay station with own material
 Here is an example of belay station with own material.
 
 
-![](../../../assets/08a450cb-37b0-4309-ac3a-dff1fd65d005.jpeg)
+![](../../../assets/08a450cb-37b0-4309-ac3a-dff1fd65d005.png)
 
-![](../../../assets/7062c1ee-4410-42ce-bd64-7ec7b70efe36.jpeg)
+![](../../../assets/7062c1ee-4410-42ce-bd64-7ec7b70efe36.png)
