73130b5a173d17c12343ddfda8bba17b2caae5d1
Daniel Herman <daniel.herman@protonmail.com>
2024-12-02 15:01:53 +0100

    Fixing config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import image from '@astrojs/image'
 
 // https://astro.build/config
 export default defineConfig({
